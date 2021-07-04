6ef7632d3008c6c8607d0251dfaf2e945be5db29
jiwon709 <jimong8702@naver.com>
2021-07-04 22:54:26 +0900

    Exclude questions that haven't been published yet

--- 05_week/spartatdd/polls/tests.py
+++ 05_week/spartatdd/polls/tests.py
@@ -59,3 +59,32 @@ class QuestionIndexViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "[New]")
         self.assertQuerysetEqual(response.context['question_list'], [question])
+
+    def test_past_question(self):
+        """
+        Question with a pub_date in the past are displayed on the
+        index page
+        """
+        past_time = timezone.now() + datetime.timedelta(days=-30)
+        question = Question(question_text="test", pub_date=past_time)
+        question.save()
+
+        response = self.client.get("/polls/")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['question_list'], [question])
+
+    def test_future_question(self):
+        """
+        Question with a pub_date in the past are displayed on the
+        index page
+        """
+        future_time = timezone.now() + datetime.timedelta(days=30)
+        question = Question(question_text="test", pub_date=future_time)
+        question.save()
+
+        response = self.client.get("/polls/")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "등록된 설문조사가 없습니다.")
+        self.assertQuerysetEqual(response.context['question_list'], [])
--- 05_week/spartatdd/polls/views.py
+++ 05_week/spartatdd/polls/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
+from django.utils import timezone
 # Create your views here.
 
 
 def index(request):
-    question_list = Question.objects.all()
-    output = ', '.join([q.question_text for q in question_list])
+    question_list = Question.objects.all().filter(
+        pub_date__lte=timezone.now()
+    )
+
     context = {'question_list': question_list}
     return render(request, 'polls/index.html', context)
