250e33be126f0166ed536a5999e2c3a805c19afc
jiwon709 <jimong8702@naver.com>
2021-07-04 22:39:15 +0900

    Add new mark to published_recently method  & Add New mark to published recently question

--- 05_week/spartatdd/polls/tests.py
+++ 05_week/spartatdd/polls/tests.py
@@ -1,6 +1,39 @@
 from django.test import TestCase
-
+from django.utils import timezone
+from .models import Question
+from .models import Choice
+import datetime
 # Create your tests here.
+
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pubdate
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False for questions whose pubdate
+        is older than 1 day.
+        """
+        time = timezone.now() + datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() returns True for questions whose pubdate
+        is in the future.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
 
 class QuestionIndexViewTests(TestCase):
     def test_has_no_questions(self):
@@ -11,3 +44,18 @@ class QuestionIndexViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "등록된 설문조사가 없습니다.")
         self.assertQuerysetEqual(response.context['question_list'], [])
+
+    def test_published_recently_has_new_mark(self):
+        """
+        Questions with a recent pub_date are displayed on the
+        index page with [New] Mark.
+        """
+        recent_time = timezone.now() - datetime.timedelta(hours=8)
+        question = Question(question_text="test", pub_date=recent_time)
+        question.save()
+
+        response = self.client.get("/polls/")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "[New]")
+        self.assertQuerysetEqual(response.context['question_list'], [question])
